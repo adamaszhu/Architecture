9a8b7b0a837bc2bc237d1443c86095ec119777ae
Adamas <azhu@transurban.com>
2021-06-25 13:48:42 +1000

    Replace the default scene with an app coordinator

---
+++ Architecture/AppCoordinator.swift
@@ -0,0 +1,41 @@
+//
+//  AppCoordinator.swift
+//  Architecture
+//
+//  Created by Adamas on 25/6/21.
+//
+
+import Foundation
+import UIKit
+
+final class AppCoordinator: Coordinator {
+
+    private let window: UIWindow
+    private let navigationController: UINavigationController
+
+    init(window: UIWindow, navigationController: UINavigationController) {
+        self.window = window
+        self.navigationController = navigationController
+        window.configure(with: navigationController)
+    }
+
+    convenience init() {
+        self.init(window: UIWindow(), navigationController: UINavigationController())
+    }
+
+    func start() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        guard let viewController = storyboard.instantiateInitialViewController() as? MainViewController else {
+            return
+        }
+        navigationController.pushViewController(viewController, animated: true)
+    }
+}
+
+private extension UIWindow {
+
+    func configure(with navigationController: UINavigationController) {
+        makeKeyAndVisible()
+        rootViewController = navigationController
+    }
+}
---
+++ Architecture/General/Coordinator.swift
@@ -0,0 +1,12 @@
+//
+//  Coordinator.swift
+//  Architecture
+//
+//  Created by Adamas on 25/6/21.
+//
+
+import Foundation
+
+protocol Coordinator {
+    func start()
+}
